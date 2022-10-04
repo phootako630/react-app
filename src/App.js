5dd146118ed64dff547b8b03dc00c8b06458c156
ivanzhang <ivanzhang220820@gmail>
2022-10-03 21:28:18 -0400

    interface and hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {BiCalendar, BiTrash} from 'react-icons/bi'
+import Search from "./component/Search";
+import AddAppointment from "./component/AddAppointment"
+import appointmentList from "./data.json"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="tex-5xl mb-3">
+          <BiCalendar className="inline-block text-red-400 align-top" />Your Appointment</h1>
+        <AddAppointment />
+        <Search />
+      <ul className= "divide-y divide-gray-200">
+          {appointmentList.map(app =>
+              <li className="px-3 py-3 flex items-start">
+                  <button type="button"
+                          className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                      <BiTrash /></button>
+                  <div className="flex-grow">
+                      <div className="flex items-center">
+                          <span className="flex-none font-medium text-2xl text-blue-500">{app.petName}</span>
+                          <span className="flex-grow text-right">{app.aptDate}</span>
+                      </div>
+                      <div><b className="font-bold text-blue-500">Owner:</b> {app.ownerName}</div>
+                      <div className="leading-tight">{app.aptNotes}</div>
+                  </div>
+              </li>
+          )}
+      </ul>
     </div>
   );
 }
